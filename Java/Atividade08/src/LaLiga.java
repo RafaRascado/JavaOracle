04fa35cd83bb8f1bded93d3faefa4cfbe090ac74
Rafa Rascado <129122750+RafaRascado@users.noreply.github.com>
2023-06-20 19:14:42 -0300

    Delete Java/Atividade08 directory

--- Java/Atividade08/src/LaLiga.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class LaLiga {
-    ArrayList<jogo> listajogo = new ArrayList<jogo>();
-    ArrayList<Equipe> equipeList = new ArrayList<Equipe>();
-    ArrayList<String> EquipeNome = new ArrayList<String>();
-}
